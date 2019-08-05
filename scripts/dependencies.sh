c7e5a8591a65b37965eb3d0bec3acd8147454ada
Nathan Gendron <nathan.gendron@gcmi.ca>
2019-08-05 13:26:28 -0400

    edited some scripts

--- scripts/dependencies.sh
+++ scripts/dependencies.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 
-go get -u -v github.com/GeertJohan/go.rice
 go get -u -v github.com/GeertJohan/go.rice/rice
-
 sudo npm i -g hogan.js
+
 sudo chown --changes --recursive $USER:$USER $HOME
